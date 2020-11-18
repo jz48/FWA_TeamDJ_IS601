1aa1bfa98fc6fc093991b064c4356608245555c2
tabzhangjx <tabzhangjx@gmail.com>
2020-11-18 10:32:46 +0800

    UPDATE: update new rating function

--- app/app.py
+++ app/app.py
@@ -53,16 +53,14 @@ def form_update_post(title):
 
 @app.route('/mrs/new', methods=['GET'])
 def form_insert_get():
-    return render_template('new.html', title='New City Form')
+    return render_template('new.html', title='New Movie Rating Form')
 
 
 @app.route('/mrs/new', methods=['POST'])
 def form_insert_post():
     cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
-                 request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
-                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'))
-    sql_insert_query = """INSERT INTO MovieRating (fldName,fldLat,fldLong,fldCountry,fldAbbreviation,fldCapitalStatus,fldPopulation) VALUES (%s, %s,%s, %s,%s, %s,%s) """
+    inputData = (request.form.get('title'), request.form.get('score'), request.form.get('year'))
+    sql_insert_query = """INSERT INTO MovieRating (Title, Score, Year) VALUES (%s, %s,%s) """
     cursor.execute(sql_insert_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
