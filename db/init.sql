5f9e4e9858b553ddebbe18c9ad62439cc6e9a6b3
Krishna Eleti <krishnaeleti@gmail.com>
2020-12-05 07:50:30 -0500

    Feature: fixed login and registration still much more to do

--- db/init.sql
+++ db/init.sql
@@ -1,18 +1,31 @@
-CREATE DATABASE LoginData;
+CREATE Database LoginData;
 use LoginData;
 
-DROP TABLE if exists Accounts;
+DROP TABLE if exists accounts;
 
-CREATE TABLE IF NOT EXISTS Accounts (
+CREATE TABLE IF NOT EXISTS accounts (
     `id` INT AUTO_INCREMENT,
-    `First_Name` VARCHAR(60) NOT NULL,
-    `Last_Name` VARCHAR(60) NOT NULL,
-    `Email` VARCHAR(100) NOT NULL,
-    `Password` VARCHAR(33) NOT NULL,
-    `Verified` INT,
+    `username` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
     PRIMARY KEY (`id`)
 );
-INSERT INTO Accounts (First_Name, Last_Name, Email, Password, Verified) VALUES
-    ('FN', 'LN', 'EM@gmail.com', 'PW', '1'),
-    ('Krishna', 'Eleti', 'kve5@njit.edu', 'Password', '1');
+INSERT INTO accounts(username, password, email) VALUES
+    ('admin','helloworld', 'krishnaeleti@gmail.com'),
+    ('admin2', 'helloworld2','kve5@njit.edu');
 
+
+CREATE TABLE IF NOT EXISTS MovieRating(
+  Year INT(11),
+  Score DECIMAL(10, 2),
+  Title VARCHAR(100)
+);
+/* INSERT QUERY */
+INSERT INTO MovieRating(Year, Score, Title) VALUES
+  (1968, 86, 'Greetings'),
+  (1970, 17, 'BloodyMama'),
+  (1970, 73, 'Hi,Mom!'),
+  (1971, 40, 'BorntoWin'),
+  (1973, 98, 'MeanStreets'),
+  (1973, 88, 'BangtheDrumSlowly'),
+  (1974, 97, 'TheGodfather,PartII'),
